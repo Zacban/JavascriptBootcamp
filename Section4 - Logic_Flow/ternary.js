addadc0b43300c2f6196fbf3afe7cb594c7d1c43
Jesper Raundahl <zacban@gmail.com>
2022-08-24 11:17:49 +0200

    initial commit

---
+++ Section4 - Logic_Flow/ternary.js
@@ -0,0 +1,10 @@
+let num = 6;
+
+// if statement
+if (num === 7)
+    console.log('lucky')
+else
+    console.log('unlucky');
+
+console.log('--TERNARY--')
+num === 7 ? console.log('lucky') : console.log('unlucky');
