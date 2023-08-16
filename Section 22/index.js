dfc271f368a82e6725067281009a446bc6e84953
Jesper Raundahl <zacban@gmail.com>
2023-08-16 14:56:58 +0200

    Video 283

--- Section 22/index.js
+++ Section 22/index.js
@@ -2,7 +2,10 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 
 const worldWidth = 800;
 const worldHeight = 800;
-const wallThickness = 40
+const wallThickness = 40;
+
+const gridWidth = 3;
+const gridHeight = 3;
 
 const engine = Engine.create();
 
@@ -31,3 +34,67 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// Maze generation
+
+const shuffle = (arr) => {
+    let counter = arr.length;
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter--);
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+
+        return arr;
+    }
+}
+const grid = Array(gridHeight)
+    .fill(null)
+    .map(() => Array(gridWidth).fill(false));
+
+const vertical = Array(gridHeight)
+    .fill(null)
+    .map(() => Array(gridWidth - 1).fill(false));
+
+const horizontal = Array(gridHeight - 1)
+    .fill(null)
+    .map(() => Array(gridWidth).fill(false));
+
+const startRow = Math.floor(Math.random() * gridHeight);
+const startCol = Math.floor(Math.random() * gridWidth);
+
+const stepCell = (row, column) => {
+    // if we already visited this cell, then return
+    if (grid[row][column] === true) {
+        return;
+    }
+
+    // set value to true to indicate the cell has been visited.
+    grid[row][column] = true;
+
+    // assemble randomly-ordered list of neighbours
+    const neighbours = shuffle([
+        [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        [row + 1, column, 'down'],
+        [row, column - 1, 'left']
+    ]);
+
+    // for each neighbour
+    for (let neighbour of neighbours) {
+        const [nextRow, nextColumn, direction] = neighbour;
+
+        if (nextRow < 0 || nextRow >= gridHeight || nextColumn < 0 || nextColumn >= gridWidth)
+            continue;
+
+        if (grid[nextRow][nextColumn])
+            continue;
+
+            if (direction)
+    }
+
+    console.log(`neighbours to [${row}][${column}]`, neighbours);
+}
+
+stepCell(startRow, startCol);
